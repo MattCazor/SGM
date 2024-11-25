bbc63f8c1030c3795ea5355e0fbf9c8d2a5053e3
isen <louis.durand@isen.yncrea.fr>
2024-11-25 14:44:44 +0100

    vrnc,,sdk

---
+++ quai.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef enum {
+    PASSAGER,
+    MARCHANDISE,
+    PETROLIER,
+    YACHT
+} TYPE_NAVIRE;
+
+typedef enum {
+    EN_MER,
+    ACCOSTE,
+    EN_ATTENTE
+} ETAT_NAVIRE;
+
+typedef struct Navire {
+    int identifiant;            
+    TYPE_NAVIRE type;           
+    ETAT_NAVIRE etat;           
+    float capacite_chargement;  
+    struct Navire* suiv;     
+} Navire;
+
+typedef struct Quai {
+    int numero;
+    float taille;              
+    float profondeur;            
+    TYPE_NAVIRE type_autorise;   
+    int capacite_max;         
+} Quai;
+
+
+
