9c2a0de6197b5fb4b69d5c971c4a04aaae7dc735
isen <louis.durand@isen.yncrea.fr>
2024-12-03 21:21:13 +0100

    yeunc

--- quai.c
+++ quai.c
@@ -2,43 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "quai.h"
+
 #define NB_QUAIS 4
-
-typedef enum 
-{
-    PASSAGER,
-    MARCHANDISE,
-    PETROLIER,
-    YACHT
-} TYPE_NAVIRE;
-
-typedef enum 
-{
-    EN_MER,
-    ACCOSTE,
-    EN_ATTENTE
-} ETAT_NAVIRE;
-
-typedef struct Navire Navire;
-struct Navire
-{
-    int identifiant;            
-    TYPE_NAVIRE type;           
-    ETAT_NAVIRE etat;           
-    float capacite_chargement;  
-    Navire* suiv;     
-};
-
-typedef struct Quai Quai;
-struct Quai
-{
-    int numero;
-    float taille;              
-    float profondeur;            
-    TYPE_NAVIRE type_autorise;   
-    int capacite_max; 
-    Navire* attente;        
-}; 
 
 char* typenavire(TYPE_NAVIRE type) 
 {
